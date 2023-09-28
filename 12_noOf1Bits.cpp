377d748fdbba67692bf92c2c84520e5fc5f72e03
RaunakScarlet <raunakscarlet@gmail.com>
2023-09-28 23:31:55 +0530

    approche second

--- 12_noOf1Bits.cpp
+++ 12_noOf1Bits.cpp
@@ -13,3 +13,25 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+
+// Second appeoches
+
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        int ans = 0;
+        while (n != 0)
+        {
+            n = n & (n - 1);
+            ans++;
+        }
+        return ans;
+    }
+};
